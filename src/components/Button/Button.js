5a43b0ab383d575c42bda36e838eae8eae6e4f9d
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-01-30 22:14:49 +0200

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,6 @@
 export const Button= ({onLoadMore}) => {
     return (
-<button className="Button" type="submit" onClick={onLoadMore}></button>
+<button className="Button" type="submit" onClick={onLoadMore}> Load more</button>
 
     )
 }
